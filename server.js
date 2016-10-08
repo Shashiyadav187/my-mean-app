c8ad411ef1d14f46f67f31e238b50cabdea0b107
P. Mohan <ngapigyi@gmail.com>
2016-10-08 11:52:50 +0000

    Add domain to handle error

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 'use strict';
 
 var app = require('./config/lib/app');
-app.start();
+
+// Deprecated
+var domain = require('domain').create();
+
+// Domain emits 'error' when it's given an unhandled error
+domain.on('error', function (err) {
+  console.log('Some unexpected error occurs.');
+  console.log(err.stack);
+  try {
+    var killtimer = setTimeout(function () { process.exit(1); }, 3000);
+    killtimer.unref();
+  } catch (er2) {
+    console.log('Internal server error 500!', er2.stack);
+  }
+});
+
+domain.run(function () {
+  app.start();
+});
